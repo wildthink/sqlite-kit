8688d5b3e87871b2b88b454644e2adae84255b71
Jason Jobe <box2019@jasonjobe.com>
2021-10-19 22:29:29 -0400

    package cleanup

--- Package.swift
+++ Package.swift
@@ -12,15 +12,9 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "/Users/jason/OpenSource/db/vapor/sql-kit"),
-//        .package(path: "../sqlite-nio"),
-//        .package(url: "https://github.com/vapor/sqlite-nio.git", from: "1.0.0"),
-//        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0"),
-//        .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0"),
     ],
     targets: [
         .target(name: "SQLiteKit", dependencies: [
-//            .product(name: "AsyncKit", package: "async-kit"),
-//            .product(name: "SQLiteNIO", package: "sqlite-nio"),
             .product(name: "SQLKit", package: "sql-kit"),
         ]),
 //        .testTarget(name: "SQLiteKitTests", dependencies: [
